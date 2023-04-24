daef8d5f6cb1c7bb2a611361a056b4e542a71785
Jacquelyn Collas <jackie@moreira.com>
2023-04-24 15:16:21 -0500

    added locations generator

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,35 @@
 // javascript
 
+const locations = [
+    "Aenucoya","Dreodora","Strikoth","Oyapios","Lunahold"
+]
+
 function randomString(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
     return item;
 }
 
-function actionDots() {
+function characterTime() {
     const actions = [
         "force","force","strike","strike","move","move","sneak","sneak","craft","craft","talk","talk","sense","sense","focus","focus"
     ];
-    let text = "";
+    let dotsText = "";
     for (i=0; i<=4; i++) {
         const dots = randomString(actions);
         const polka = actions.indexOf(dots);
         actions.splice(polka, 1);
         if (i < 4) {
-            text += dots + ", ";
+            dotsText += dots + ", ";
         } else {
-            text += dots;
+            dotsText += dots;
         }
     }
 
-    document.getElementById("action-dots").innerHTML = text;
+    document.getElementById("action-dots").innerHTML = dotsText;
+
+    const place = randomString(locations);
+    console.log(place);
+
+    document.getElementById("locations-time").innerHTML = place;
 }
